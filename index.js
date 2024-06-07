af9d1d9579b7ae6d61602360c48a1663847180ed
kaiiff <kaifmansuri40@gmail.com>
2024-06-07 12:19:11 +0530

    noon second commit

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ dotenv.config();
 
 const path = require("path");
 const bodyParser = require("body-parser");
+const helmet = require("helmet");
 const port = process.env.PORT || 4006;
 const userRouter = require("./routers/userRouter");
-
 
 // db call here
 const myDb = require("./connection/db");
@@ -16,12 +16,14 @@ myDb();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//to protect our app from vulnerabilities
+app.use(helmet());
+
 // static files handle here
 app.use("/public/images", express.static("public/images"));
 
 // router handle here
 app.use("/user", userRouter);
-
 
 app.listen(port, () => {
   console.log(`Server start on ${port} successfully!`);
